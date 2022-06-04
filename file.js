acd1833264133f99709e303e129049fcaf457c2f
Gideon uwa <uwagideon092@gmail.com>
2022-06-04 10:32:36 -0700

    learnt_how_to_get_blob_from_cloud_storage

--- file.js
+++ file.js
@@ -1,4 +1,23 @@
+import { storage } from "./firebase.config";
+import { ref, uploadBytes, getBlob } from "@firebase/storage";
 const button = document.querySelector(".submitFile");
-const inputFile = document.querySelector("input[type=file]").files.item(0);
-
-button.addEventListener("click", () => {});
+let get;
+const imagesRef = ref(storage, "images/foodies");
+console.log(imagesRef.bucket);
+button.addEventListener("click", () => {
+  const reader = new FileReader();
+  const inputFile = document.querySelector("input[type=file]").files.item(0);
+  // const value = reader.readAsDataURL(inputFile);
+  reader.onload = function() {
+    console.log(this.result);
+  };
+  // console.log(storage);
+  // console.log(inputFile);
+  // uploadBytes(imagesRef, inputFile).then(() => {
+  //   console.log("file uploaded successfully");
+  // });
+  getBlob(imagesRef).then((blobFile) => {
+    console.log(blobFile);
+    reader.readAsDataURL(blobFile);
+  });
+});
